c3c3f5e8c1c7183376f787907d6b3f45aeba515a
Ravi Soni <rv.soni@hotmail.com>
2026-05-15 12:39:05 +0530

    Update test-ruff.py

--- test-ruff.py
+++ test-ruff.py
@@ -4,6 +4,7 @@ import sys
 def My_Function(n): # N802: Function name should be lowercase
     X = 10          # F841: Local variable 'X' is assigned but never used
     print(f"Result: {n}")
+    y = 11
     return result   # F821: Undefined name 'result' (should be 'n')
 
 if __name__ == "__main__":
